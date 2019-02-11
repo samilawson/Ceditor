806c717cc470d6567e79c2229e2a2fdf55c65b33
Sam Lawson <samilawson17@gmail.com>
2019-02-11 07:11:45 -0600

    press ctrl q to quit

--- Ceditor.c
+++ Ceditor.c
@@ -6,6 +6,9 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 struct termios orig_termios;
 
@@ -61,8 +64,7 @@ int main()
         {
             printf("%d ('%c')\r\n", c, c);
         }
-        if (c == 'q')
-            break;
+        if (c == CTRL_KEY('q')) break;
     }
     return 0;
 }

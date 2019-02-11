e8b7c6171686918189178cde45b2ee07c1ab3089
Sam Lawson <samilawson17@gmail.com>
2019-02-10 21:14:16 -0600

    edits

--- Ceditor.c
+++ Ceditor.c
@@ -5,24 +5,29 @@
 struct termios orig_termios;
 
 /* Disable raw mode at exit */
-void disableRawMode(){
+void disableRawMode()
+{
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 /* Turn off echoing */
-void enableRawMode(){
+void enableRawMode()
+{
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
+    /* Turns off canonical mode, which reads inout byte by byte instead of line by line */
+    raw.c_lflag &= ~(ECHO | ICANON);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-int main(){
+int main()
+{
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+        ;
     return 0;
 }

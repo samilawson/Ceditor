0ea663278ff083ba8e63aa7c2a5d04b5d65a8dbc
Sam Lawson <samilawson17@gmail.com>
2019-02-11 07:09:49 -0600

    sectioning

--- Ceditor.c
+++ Ceditor.c
@@ -1,3 +1,4 @@
+/*** includes ***/
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,8 +6,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
 struct termios orig_termios;
 
+/*** terminal ***/
 /* Error handling */
 void die(const char *s)
 {
@@ -40,6 +43,7 @@ void enableRawMode()
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+/*** init ***/
 int main()
 {
     enableRawMode();

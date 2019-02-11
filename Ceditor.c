191ce088087e5046e260053ab9c9145ac291b185
Sam Lawson <samilawson17@gmail.com>
2019-02-10 20:32:32 -0600

    enable and disable raw mode

--- Ceditor.c
+++ Ceditor.c
@@ -1,7 +1,28 @@
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+/* Disable raw mode at exit */
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+/* Turn off echoing */
+void enableRawMode(){
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main(){
+    enableRawMode();
+
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1);
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }

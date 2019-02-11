7ff0e5fd2805c724d9afb1ab11f25e53caa89880
Sam Lawson <samilawson17@gmail.com>
2019-02-11 07:22:03 -0600

    clear screen at exit

--- Ceditor.c
+++ Ceditor.c
@@ -16,6 +16,9 @@ struct termios orig_termios;
 /* Error handling */
 void die(const char *s)
 {
+    write(STDOUT_FILENO, "\x1b[2j", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -62,6 +65,7 @@ char editorReadKey()
 /*** output ***/
 void editorRefreshScreen(){
     write(STDOUT_FILENO, "\x1b[2j", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 /*** input ***/
 void editorProcessKeypress()
@@ -71,6 +75,8 @@ void editorProcessKeypress()
     switch (c)
     {
         case CTRL_KEY('q');
+        write(STDOUT_FILENO, "\x1b[2j", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
     }

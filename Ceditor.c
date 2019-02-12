f30b7d4eff2f5738c75b12e88f4dc3430ed9522e
Sam Lawson <samilawson17@gmail.com>
2019-02-12 12:39:44 -0600

    store rows

--- Ceditor.c
+++ Ceditor.c
@@ -27,11 +27,20 @@ enum editorKey
 };
 
 /*** data ***/
+
+typedef struct erow {
+    int size;
+    char *chars;
+
+} erow;
+
 struct editorConfig
 {
     int cx, cy;
     int screenrows;
     int screencols;
+    int numrows;
+    erow row;
     struct termios orig_termios;
 };
 
@@ -331,6 +340,7 @@ void initEditor()
 {
     E.cx = 0;
     E.cy = 0;
+    E.numrows = 0;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWindowSize");

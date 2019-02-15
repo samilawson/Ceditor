696c653550daf828c1ba602018c7a76f930e28fc
Sam Lawson <samilawson17@gmail.com>
2019-02-15 09:34:04 -0600

    multiple lines

--- Ceditor.c
+++ Ceditor.c
@@ -47,7 +47,7 @@ struct editorConfig
     int screenrows;
     int screencols;
     int numrows;
-    erow row;
+    erow *row;
     struct termios orig_termios;
 };
 
@@ -403,7 +403,8 @@ void initEditor()
     E.cx = 0;
     E.cy = 0;
     E.numrows = 0;
-
+    E.row = NULL;
+    
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWindowSize");
 }
